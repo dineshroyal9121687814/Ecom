c888aa6b7ae4d3d8372c644c928b7974c05aa63c
dineshroyal9121687814 <85698452+dineshroyal9121687814@users.noreply.github.com>
2025-05-16 00:19:49 +0530

    Update settings.py

--- Ecom/settings.py
+++ Ecom/settings.py
@@ -67,7 +67,7 @@ WSGI_APPLICATION = 'Ecom.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.environ.get('SQLITE_PATH', BASE_DIR / 'db.sqlite3'),
     }
 }
 
